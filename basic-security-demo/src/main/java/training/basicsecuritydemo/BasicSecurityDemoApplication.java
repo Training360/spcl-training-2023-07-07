df48c0b1e8775c0f1201061cb8bfe31d1f94224a
Viczián István <viczian.istvan@gmail.com>
2023-07-07 10:50:12 +0200

    Init

---
+++ basic-security-demo/src/main/java/training/basicsecuritydemo/BasicSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package training.basicsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSecurityDemoApplication.class, args);
+	}
+
+}
---
+++ basic-security-demo/src/test/java/training/basicsecuritydemo/BasicSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package training.basicsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
