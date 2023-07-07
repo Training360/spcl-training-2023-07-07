69f8c097e96cd291895797bba59ec8afcf08ba71
Viczián István <viczian.istvan@gmail.com>
2023-07-07 12:36:49 +0200

    Config

--- basic-security-demo/src/main/java/training/basicsecuritydemo/SecurityConfig.java
+++ basic-security-demo/src/main/java/training/basicsecuritydemo/SecurityConfig.java
@@ -2,12 +2,15 @@ package training.basicsecuritydemo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -32,5 +35,20 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(spec -> spec
+                        // Most mégsem lesz végtelenciklus?
+//                                .pathMatchers("/login", "/logout").permitAll()
+                                .pathMatchers("/").authenticated()
+                                .pathMatchers("/users").hasRole("ADMIN")
+                                .anyExchange().hasAnyRole("USER", "ADMIN")
+                        )
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults());
+        return http.build();
     }
 }
