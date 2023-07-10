c6e0d60feabdd23126da57151615d0bf5467f848
Viczián István <viczian.istvan@gmail.com>
2023-07-10 15:15:16 +0200

    Config server

---
+++ config-server-demo/src/test/java/training/configserverdemo/ConfigServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package training.configserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
