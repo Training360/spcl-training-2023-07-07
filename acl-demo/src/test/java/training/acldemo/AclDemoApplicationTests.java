ea531d6b102ae1eb4ec3e3a4a3ea4f758276b1c0
Viczián István <viczian.istvan@gmail.com>
2023-07-10 13:17:29 +0200

    ACL

---
+++ acl-demo/src/test/java/training/acldemo/AclDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package training.acldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AclDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
