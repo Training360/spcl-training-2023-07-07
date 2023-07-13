16b90be84bdb7d03734d81e20372724921f11892
Viczián István <viczian.istvan@gmail.com>
2023-07-13 09:47:18 +0200

    Storm signal init

---
+++ storm-signal/src/main/java/training/stormsignal/StormSignalApplication.java
@@ -0,0 +1,13 @@
+package training.stormsignal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StormSignalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StormSignalApplication.class, args);
+	}
+
+}
---
+++ storm-signal/src/test/java/training/stormsignal/StormSignalApplicationTests.java
@@ -0,0 +1,13 @@
+package training.stormsignal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StormSignalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
