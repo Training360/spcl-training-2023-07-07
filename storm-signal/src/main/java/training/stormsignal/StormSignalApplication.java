c076b602cd56eb46aa861fd9ae4b5b10d051e9e4
Viczián István <viczian.istvan@gmail.com>
2023-07-13 09:59:03 +0200

    Hello world

--- storm-signal/src/main/java/training/stormsignal/StormSignalApplication.java
+++ storm-signal/src/main/java/training/stormsignal/StormSignalApplication.java
@@ -1,13 +1,30 @@
 package training.stormsignal;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @SpringBootApplication
+@Slf4j
 public class StormSignalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StormSignalApplication.class, args);
+	}
+
+	@Bean
+	public Supplier<Mono<String>> createHelloMessage() {
+		return () -> Mono.just("hello world");
+	}
+
+	@Bean
+	public Consumer<Mono<String>> logHelloMessage() {
+		return stringMono -> stringMono.subscribe(s -> log.debug("Message has come: {}", s));
 	}
 
 }
